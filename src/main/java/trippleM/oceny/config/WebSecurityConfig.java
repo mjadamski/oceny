e767b5a62cf48cd660bdf561e1b9dccb74444e00
michal.a <mj_adamski@wp.pl>
2018-09-01 20:15:56 +0200

    html works!

---
+++ src/main/java/trippleM/oceny/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package trippleM.oceny.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/").hasAnyAuthority("ROLE_USER")
+                .anyRequest().permitAll()
+                .and().csrf().disable()
+                .formLogin()
+                .loginPage("/login_temp")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login-process")
+                .failureUrl("/user/login?error")
+                .defaultSuccessUrl("/profile");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(
+                        "SELECT email, password, enabled FROM user WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT email, role FROM user WHERE email = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+}
---
+++ src/main/java/trippleM/oceny/controllers/MainController.java
@@ -0,0 +1,30 @@
+package trippleM.oceny.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(){
+        return "index";
+    }
+
+    @RequestMapping("/logowanie")
+    public String logowanie(){
+        return "logowanie";
+    }
+
+    @RequestMapping("/rejestracja")
+    public String rejestracja(){
+        return "rejestracja";
+    }
+
+    @RequestMapping("/profil")
+    public String profil(){
+        return "profil";
+    }
+
+
+}
