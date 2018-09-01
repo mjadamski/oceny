1ed5651a4a3f0243a98cd4fbd1648f8f9abdc98d
michal.a <mj_adamski@wp.pl>
2018-09-01 19:13:08 +0200

    another first commit

---
+++ src/main/java/trippleM/oceny/OcenyApplication.java
@@ -0,0 +1,12 @@
+package trippleM.oceny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcenyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcenyApplication.class, args);
+	}
+}
